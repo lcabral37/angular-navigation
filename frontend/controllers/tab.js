68db027ffae3d8c2fd355b0a6d9d1d5d02c849e1
Luis Cabral <lcabral37@gmail.com>
2015-12-14 01:06:25 +0100

    use ng-repeat for the tabs rendering

--- frontend/controllers/tab.js
+++ frontend/controllers/tab.js
@@ -4,6 +4,12 @@ angular.module('tabsSample')
   .controller('tabCtrl',
     ['$scope', '$location', '$route',
   function($scope, $location) {
+    $scope.tabs = [
+      {heading: 'About', route: '/about'},
+      {heading: 'Register', route: '/register'},
+      {heading: 'Confirm', route: '/confirm'},
+      {heading: 'Complete', route: '/complete'}
+    ];
 
     $scope.isActive = function(route) {
       return route === $location.path();
